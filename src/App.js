ea87bb84fe78179ff9d5ebed33312d9f3f9d4a41
skamok <sk_amok@mail.ru>
2022-07-06 18:03:12 +0300

    rebuild pizza block

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
+import pizzas from './assets/pizzas.json';
+
 
 function App() {
+  console.log(pizzas)
   return (
     <div className="App">
       <div className="wrapper">
@@ -18,8 +21,7 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              <PizzaBlock title="Mexico" price={500} />
-              <PizzaBlock title="Hot" price={300} />
+              {pizzas.map((pizza, index) => (<PizzaBlock key={index} title={pizza.title} price={pizza.price} imageUrl={pizza.imageUrl} sizes={pizza.sizes} types={pizza.types} />))}
             </div>
           </div>
         </div>
