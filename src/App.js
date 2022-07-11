31b5568170d8d882b830a8d9489d3253a7349707
skamok <sk_amok@mail.ru>
2022-07-11 09:33:37 +0300

    add Skeleton for pizza

--- src/App.js
+++ src/App.js
@@ -5,17 +5,23 @@ import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
+import PizzaBlockSkeleton from './components/PizzaBlock/PizzaBlockSkeleton';
 // import pizzas from './assets/pizzas.json';
 
 
 function App() {
 
+  // React skeleton use for blank fake data
   const [pizzas, setPizzas] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
     fetch('https://62cbabe13e924a012869fe7d.mockapi.io/items')
       .then((res) => res.json())
-      .then((data) => setPizzas(data));
+      .then((data) => {
+        setPizzas(data);
+        setIsLoading(false);
+      });
   }, []);
 
 
@@ -31,7 +37,12 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              {pizzas.map((pizza, index) => (<PizzaBlock key={pizza.id} title={pizza.name} price={pizza.price} imageUrl={pizza.imageUrl} sizes={pizza.sizes} types={pizza.types} />))}
+              {
+                isLoading ?
+                  [...new Array(6)].map((_, index) => <PizzaBlockSkeleton key={index} />)
+                  :
+                  pizzas.map((pizza) => (<PizzaBlock key={pizza.id} title={pizza.name} price={pizza.price} imageUrl={pizza.imageUrl} sizes={pizza.sizes} types={pizza.types} />))
+              }
             </div>
           </div>
         </div>
---
+++ src/components/PizzaBlock/index.js
@@ -0,0 +1,4 @@
+export { default } from './PizzaBlock';
+
+// Smart trick for webpack
+// when import PizzaBlock from 'Pizzablock' webpack wants file PizzaBlock.jsx or folder PizzaBlock with index.js file
